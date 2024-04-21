787b5d25880c76bc21527483579f19aa4e8ef603
CheekyGeroin <dan.97198@gmail.com>
2024-04-22 01:19:42 +0200

    done tasks

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,24 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  Mon = 'monday',
+  Tue = 'tuesday',
+  Wed = "wednesday",
+  Thu = "thursday",
+  Fri = 'friday',
+  Sat = 'saturday',
+  Sun = "sunday"
+
+}
+
+const isWeekend = (day:WeekDays):boolean => {
+  if (day === WeekDays.Sat || day === WeekDays.Sun) {
+    return true
+  } else {
+    return false
+  }
+
+}
+
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,7 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+const page1:FirstPage = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,11 +13,35 @@ const page1 = {
   }
 }
 
-const page2 = {
+
+type FirstPage = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: string
+  details: {
+    createAt: Date, updateAt: Date
+  }
+}
+
+const page2:SecondPage = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
+}
+
+type SecondPage = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status:string
+}
+
+type TwoPages = FirstPage & SecondPage;
+
+const Pages: TwoPages = {
+  ...page1, ...page2
 }
 
 export {};
